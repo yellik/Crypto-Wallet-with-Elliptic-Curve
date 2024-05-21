8fbb9f135a68ec1c61b8352ee88d61006c42f3c4
Yelena Kensborn <yelenakensborn@gmail.com>
2024-05-21 13:48:59 +0200

    init repo

---
+++ utilities/crypto-lib.mjs
@@ -0,0 +1,10 @@
+import crypto from 'crypto';
+import { ec } from 'elliptic';
+
+export const createHash = (...args) => {
+  return crypto.createHash('sha256').update(args.sort().join('')).digest('hex');
+};
+
+export const ellipticHash = new ec('secp256k1');
+
+export const verifySignature = ({ publicKey, data, signature})
