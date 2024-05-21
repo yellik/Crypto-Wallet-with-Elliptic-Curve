38d121af951b466d04ab66d0c74ec2dfc41a3978
Yelena Kensborn <yelenakensborn@gmail.com>
2024-05-21 14:48:41 +0200

    added transactions tests

--- models/Transactions.mjs
+++ models/Transactions.mjs
@@ -1,7 +1,15 @@
+import { v4 as uuidv4 } from 'uuid'
 export default class Transaction {
     constructor({sender, recipient, amount}) {
-        this.id =1;
-        this.outputMap = {};
-        this.inputMap = {};
+        this.id = uuidv4().replaceAll('-', '');
+        this.outputMap = this.createOutputMap({ sender, recipient, amount});
+        this.inputMap = this.createInputMap({ sender, outputMap: this.outputMap});
     }
+
+    createOutputMap({ sender, recipient, amunt}) { 
+        const outputMap = {}
+        
+    }
+
+    createInputMap({ sender, outputMap}) {}
 }
--- tests/transaction.test.mjs
+++ tests/transaction.test.mjs
@@ -4,5 +4,27 @@ import { verifySignature } from '../utilities/crypto-lib.mjs'
 import Transaction from '../models/Transaction.mjs'
 
 describe('Transaction', () => {
-    
+    let transaction, sender, recipient, amount;
+
+    beforeEach(() => {
+        sender = new Wallet();
+        recipient: 'recipient-dummy-address';
+        amount: 50;
+
+        transaction = new Transaction({ sender, recipient, amount})
+    });
+
+    describe('properties', () => {
+
+    });
+
+    describe('outputMap()', () => {
+        it('should display the receipients balance', () => {
+            expect(transactionoutputMap[recipient]).toEqual(amount);
+        });
+
+        it('should displau the senders balance', () => {
+            expect(transaction.outputMap[sender.publicKey])
+        })
+    })
 })
